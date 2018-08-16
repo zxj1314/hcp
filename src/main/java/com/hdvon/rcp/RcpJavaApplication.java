9c0153ee59af364985b280ffa9205cdd78d9c59b
zxj <961310225@qq.com>
2018-08-16 20:26:04 +0800

    change1

--- src/main/java/com/hdvon/rcp/RcpJavaApplication.java
+++ src/main/java/com/hdvon/rcp/RcpJavaApplication.java
@@ -3,6 +3,7 @@ package com.hdvon.rcp;
 import com.hdvon.rcp.common.decrypt.PropertyPasswordDecodingContextInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
